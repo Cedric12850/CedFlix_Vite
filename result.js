b14bdb52d774e0ed6a2cd8c52819d9d1d1edac30
Cedric12850 <cedric.deswarte81@gmail.com>
2024-08-01 17:00:09 +0200

    fin de journée

--- result.js
+++ result.js
@@ -3,7 +3,7 @@ export const result = (film) => {
     let cedFlix =document.querySelector('.cedFlix');
     cedFlix.innerHTML = `
     <div>
-        <img src="${film.show.image.original}" alt="Affiche du film></img>
+        
         <div>
         <h2>${film.show.name}</h2>
         <p>${film.show.summary}</p>
